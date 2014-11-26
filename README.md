0ba10b0f461afebed9162f5901ab38a29958f586
David M. Lee <dlee@digium.com>
2014-11-26 10:48:06 -0600

    Updates to run on Yosemite
     * Updating README
     * Upgrading to asterisk-13.3.0, with a patch to fix for Yosemite
     * Use the `fails_with` flag instead of specifically trying to bring in
       gcc
     * Updated pjsip-asterisk to 2.3
     * Changed pjsip-asterisk to be keg-only, in case it ever comes back in
       upstream homebrew.
     * Use --sysconfdir so configs aren't lost on upgrades

--- README.md
+++ README.md
@@ -2,24 +2,34 @@
 This repo contains the Homebrew formulas I use for my [Asterisk][ast] dev
 machine. This is pretty much a few packages which, for one reason or
 another, aren't the best fit for going into Homebrew itself.
- 
+
 ## Installation
     xcode-select --install
     brew tap leedm777/homebrew-asterisk
     brew install asterisk
-
-## Add an asterisk group and user:
-
-    sudo dseditgroup -o create asterisk
-    sudo dscl . create /Users/asterisk
-    sudo dseditgroup -o edit -a asterisk -t user asterisk
 
 ## Make some directories not created during install
 
     mkdir -p /usr/local/Cellar/asterisk/13/etc/asterisk
     mkdir -p /usr/local/Cellar/asterisk/13/var/run/asterisk
 
-## Get the asterisk-basic configs from github 
+## Running asterisk
+
+If you want to just run Asterisk occasionally, just start it up using
+`asterisk -c`.
+
+## Running as a service
+
+To install and run Asterisk as a service, add an asterisk group and user:
+
+## Add an asterisk group and user:
+
+
+    sudo dseditgroup -o create asterisk
+    sudo dscl . create /Users/asterisk
+    sudo dseditgroup -o edit -a asterisk -t user asterisk
+
+## Get the asterisk-basic configs from github
 ## (Optional if you already have your own configs, but make for an easy start)
 
     git clone https://github.com/matt-jordan/asterisk-configs.git
@@ -35,5 +45,4 @@ another, aren't the best fit for going into Homebrew itself.
 
     launchctl load /Library/LaunchDaemons/org.asterisk.asterisk.plist
 
-[ast]: http://asterisk.org/
-[voip-info]: http://www.voip-info.org/wiki/view/Building+Asterisk+on+MacOSX
+ [ast]: http://asterisk.org/
--- asterisk.rb
+++ asterisk.rb
@@ -2,40 +2,54 @@ require 'formula'
 
 class Asterisk < Formula
   homepage 'http://www.asterisk.org'
-  url 'http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz'
-  sha1 'be622898815da65542c9a6d5639aba960d7d4798'
+  url 'http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-13.3.0.tar.gz'
+  sha1 '85e5f3ae0528280f6f6df7c191631f495dea2eaf'
 
-  depends_on 'homebrew/versions/gcc48'
+  # Asterisk depends on specific GCC features
+  fails_with :clang
+  fails_with :llvm
+
+  depends_on 'pkg-config' => :build
+
   depends_on 'gmime'
   depends_on 'iksemel'
   depends_on 'jansson'
   depends_on 'homebrew/dupes/ncurses'
   depends_on 'openssl'
   depends_on 'pjsip-asterisk'
-  depends_on 'pkg-config' => :build
   depends_on 'speex'
   depends_on 'sqlite'
   depends_on 'srtp'
   depends_on 'unixodbc'
 
   def install
+    # To help debug broken builds
+    if ARGV.verbose?
+      # disable parallel builds
+      ENV.j1
+    end
+
     openssl = Formula['openssl']
     sqlite = Formula['sqlite']
     unixodbc = Formula['unixodbc']
+    pjsip = Formula['pjsip-asterisk']
 
-    # Asterisk does not build with clang; only GCC
-    ENV["CC"]  = "/usr/local/bin/gcc-4.8"
-    ENV["CXX"] = "/usr/local/bin/g++-4.8"
     # Some Asterisk code doesn't follow strict aliasing rules
-    ENV["CFLAGS"] = "-fno-strict-aliasing"
-    ENV["PKG_CONFIG"] = "/usr/local/bin/pkg-config"
+    ENV.append "CFLAGS", "-fno-strict-aliasing"
+
+    # Use brew's pkg-config
+    ENV["PKG_CONFIG"] = "#{HOMEBREW_PREFIX}/bin/pkg-config"
 
     system "./configure", "--prefix=#{prefix}",
-                          "--without-netsnmp",
-                          "--without-gtk2",
-                          "--with-ssl=#{openssl.opt_prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--with-pjsip=#{pjsip.opt_prefix}",
                           "--with-sqlite3=#{sqlite.opt_prefix}",
-                          "--with-unixodbc=#{unixodbc.opt_prefix}"
+                          "--with-ssl=#{openssl.opt_prefix}",
+                          "--with-unixodbc=#{unixodbc.opt_prefix}",
+                          "--without-gmime",
+                          "--without-gtk2",
+                          "--without-iodbc",
+                          "--without-netsnmp"
 
     system "make", "menuselect/cmenuselect",
                    "menuselect/nmenuselect",
