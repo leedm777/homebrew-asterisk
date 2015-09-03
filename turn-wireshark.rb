576b243ac148db9162006012e1987b0624e0ec3c
David M. Lee <dlee@respoke.io>
2015-09-03 13:42:11 -0500

    Adding turn-wireshark
    This has a hack applied that allows better decoding of WebRTC/TURN
    traffic.

---
+++ turn-wireshark.rb
@@ -0,0 +1,188 @@
+# A patched Wireshark that can decode some encapsulation transports, like
+# STUN, TURN, ...
+# The patch is as yet too ugly to push upstream
+class TurnWireshark < Formula
+  desc "Graphical network analyzer and capture tool (with turn patch)"
+  homepage "https://www.wireshark.org"
+
+  conflicts_with "wireshark", :because => "It's the same thing, only patched"
+
+  stable do
+    url "https://www.wireshark.org/download/src/all-versions/wireshark-1.12.7.tar.bz2"
+    mirror "https://1.eu.dl.wireshark.org/src/wireshark-1.12.7.tar.bz2"
+    sha256 "c74a1c14e72ce0f198a93d832e71742c7f312cbbe719e5def9ecef176860f92c"
+
+    # Removes SDK checks that prevent the build from working on CLT-only systems
+    # Reported upstream: https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9290
+    patch :DATA
+
+    depends_on "homebrew/dupes/libpcap" => :optional
+  end
+
+  devel do
+    url "https://www.wireshark.org/download/src/all-versions/wireshark-1.99.8.tar.bz2"
+
+    patch do
+      url "https://gist.githubusercontent.com/leedm777/1f0fc3857e5fa7dc0f82/raw/76417cbfeb833d24d2720d21427875a899e55134/wireshark-turn-1.99.8.diff"
+      sha256 "f5177ee2b48d00c8bb6402175433ac90abcdbd5525ee48ad52743ee558335e91"
+    end
+
+    mirror "https://1.eu.dl.wireshark.org/src/wireshark-1.99.8.tar.bz2"
+    sha256 "6e03021a82cbc6b039210d37694ae51de101b7ffd4eb0e92a65ff84b4499e211"
+
+    depends_on "homebrew/dupes/libpcap" if MacOS.version == :mavericks
+  end
+
+  head do
+    url "https://code.wireshark.org/review/wireshark", :using => :git
+
+    patch do
+      url "https://gist.githubusercontent.com/leedm777/1f0fc3857e5fa7dc0f82/raw/76417cbfeb833d24d2720d21427875a899e55134/wireshark-turn-1.99.8.diff"
+      sha256 "f5177ee2b48d00c8bb6402175433ac90abcdbd5525ee48ad52743ee558335e91"
+    end
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  option "with-gtk+3", "Build the wireshark command with gtk+3"
+  option "with-gtk+", "Build the wireshark command with gtk+"
+  option "with-qt", "Build the wireshark-qt command (can be used with or without either GTK option)"
+  option "with-qt5", "Build the wireshark-qt command with qt5 (can be used with or without either GTK option)"
+  option "with-headers", "Install Wireshark library headers for plug-in development"
+
+  depends_on "pkg-config" => :build
+
+  depends_on "glib"
+  depends_on "gnutls"
+  depends_on "libgcrypt"
+  depends_on "d-bus"
+
+  depends_on "geoip" => :recommended
+  depends_on "c-ares" => :recommended
+
+  depends_on "libsmi" => :optional
+  depends_on "lua" => :optional
+  depends_on "portaudio" => :optional
+  depends_on "qt5" => :optional
+  depends_on "qt" => :optional
+  depends_on "gtk+3" => :optional
+  depends_on "gtk+" => :optional
+  depends_on "gnome-icon-theme" if build.with? "gtk+3"
+
+  def install
+    no_gui = build.without?("gtk+3") && build.without?("qt") && build.without?("gtk+") && build.without?("qt5")
+
+    args = ["--disable-dependency-tracking",
+            "--disable-silent-rules",
+            "--prefix=#{prefix}",
+            "--with-gnutls"]
+
+    args << "--disable-wireshark" if no_gui
+    args << "--disable-gtktest" if build.without?("gtk+3") && build.without?("gtk+")
+    args << "--with-qt" if build.with?("qt") || build.with?("qt5")
+    args << "--with-gtk3" if build.with? "gtk+3"
+    args << "--with-gtk2" if build.with? "gtk+"
+    args << "--with-libcap=#{Formula["libpcap"].opt_prefix}" if build.with? "libpcap"
+
+    if build.head?
+      args << "--disable-warnings-as-errors"
+      system "./autogen.sh"
+    end
+
+    system "./configure", *args
+    system "make"
+    ENV.deparallelize # parallel install fails
+    system "make", "install"
+
+    if build.with? "headers"
+      (include/"wireshark").install Dir["*.h"]
+      (include/"wireshark/epan").install Dir["epan/*.h"]
+      (include/"wireshark/epan/crypt").install Dir["epan/crypt/*.h"]
+      (include/"wireshark/epan/dfilter").install Dir["epan/dfilter/*.h"]
+      (include/"wireshark/epan/dissectors").install Dir["epan/dissectors/*.h"]
+      (include/"wireshark/epan/ftypes").install Dir["epan/ftypes/*.h"]
+      (include/"wireshark/epan/wmem").install Dir["epan/wmem/*.h"]
+      (include/"wireshark/wiretap").install Dir["wiretap/*.h"]
+      (include/"wireshark/wsutil").install Dir["wsutil/*.h"]
+    end
+  end
+
+  def caveats; <<-EOS.undent
+    To better decode WebRTC/TURN traffic, the devel and HEAD installs have a
+    very hackish patch applied to them.
+
+    If your list of available capture interfaces is empty
+    (default OS X behavior), try the following commands:
+
+      curl https://bugs.wireshark.org/bugzilla/attachment.cgi?id=3373 -o ChmodBPF.tar.gz
+      tar zxvf ChmodBPF.tar.gz
+      open ChmodBPF/Install\\ ChmodBPF.app
+
+    This adds a launch daemon that changes the permissions of your BPF
+    devices so that all users in the 'admin' group - all users with
+    'Allow user to administer this computer' turned on - have both read
+    and write access to those devices.
+
+    See bug report:
+      https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=3760
+    EOS
+  end
+
+  test do
+    system bin/"randpkt", "-b", "100", "-c", "2", "capture.pcap"
+    output = shell_output("#{bin}/capinfos -Tmc capture.pcap")
+    assert_equal "File name,Number of packets\ncapture.pcap,2\n", output
+  end
+end
+
+__END__
+diff --git a/configure b/configure
+index cd41b63..c473fe7 100755
+--- a/configure
++++ b/configure
+@@ -16703,42 +16703,12 @@ $as_echo "yes" >&6; }
+ 				break
+ 			fi
+ 		done
+-		if test -z "$SDKPATH"
+-		then
+-			{ $as_echo "$as_me:${as_lineno-$LINENO}: result: no" >&5
+-$as_echo "no" >&6; }
+-			as_fn_error $? "We couldn't find the SDK for OS X $deploy_target" "$LINENO" 5
+-		fi
+ 		{ $as_echo "$as_me:${as_lineno-$LINENO}: result: yes" >&5
+ $as_echo "yes" >&6; }
+ 		;;
+ 	esac
+
+ 	#
+-	# Add a -mmacosx-version-min flag to force tests that
+-	# use the compiler, as well as the build itself, not to,
+-	# for example, use compiler or linker features not supported
+-	# by the minimum targeted version of the OS.
+-	#
+-	# Add an -isysroot flag to use the SDK.
+-	#
+-	CFLAGS="-mmacosx-version-min=$deploy_target -isysroot $SDKPATH $CFLAGS"
+-	CXXFLAGS="-mmacosx-version-min=$deploy_target -isysroot $SDKPATH $CXXFLAGS"
+-	LDFLAGS="-mmacosx-version-min=$deploy_target -isysroot $SDKPATH $LDFLAGS"
+-
+-	#
+-	# Add a -sdkroot flag to use with osx-app.sh.
+-	#
+-	OSX_APP_FLAGS="-sdkroot $SDKPATH"
+-
+-	#
+-	# XXX - do we need this to build the Wireshark wrapper?
+-	# XXX - is this still necessary with the -mmacosx-version-min
+-	# flag being set?
+-	#
+-	OSX_DEPLOY_TARGET="MACOSX_DEPLOYMENT_TARGET=$deploy_target"
+-
+-	#
+ 	# In the installer package XML file, give the deployment target
+ 	# as the minimum version.
+ 	#
+
