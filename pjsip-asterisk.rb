0869b6cfaa54e654ef8d1fc11e8608124e4a9035
Darren Sessions <darrensessions@users.noreply.github.com>
2018-04-26 10:13:00 -0400

    Fixed disabled <<-EOF.undent error

--- pjsip-asterisk.rb
+++ pjsip-asterisk.rb
@@ -20,7 +20,7 @@ class PjsipAsterisk < Formula
     # Build for not-debug
     ENV["CFLAGS"] = "-O2 -DNDEBUG"
 
-    config_site = <<-EOF.undent
+    config_site = <<-EOF
       #include <sys/select.h>
 
       /*
